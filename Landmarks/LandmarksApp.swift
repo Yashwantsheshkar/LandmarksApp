91d9c6bad2319dcdecc464d9a53493f4f516e1c2
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-26 23:04:59 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yashwant Sheshkar on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
